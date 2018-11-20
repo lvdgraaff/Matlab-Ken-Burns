5149e1d2bc3119f42c10efd04731b41ec5d6f342
Leon van der Graaff <l.vandergraaff@tudelft.nl>
2018-11-20 08:34:34 +0100

    Improvements to demo_KenBurnsObj * use KenBurnsObj.image(); * use videoWriter.FrameRate * added example for oversampling of small images

--- demo_KenburnsObj.m
+++ demo_KenburnsObj.m
@@ -8,18 +8,17 @@ KenBurns = KenburnsObj(videoWriter, Image);
 
 % Demo
 clf;
-imshow(Image); 
-colormap gray; 
-axis image;
+KenBurns.image();
 KenBurns.plot();
 
-%% Write to output file
+% write
 KenBurns.make();
 
 %% Demonstration of options
+Image = imread('cameraman.tif');
 videoWriter = VideoWriter('kenburns_options','MPEG-4');
+videoWriter.FrameRate = 25;
 KenBurns = KenburnsObj(videoWriter, Image);
-KenBurns.fps = 25; % [1/s]
 KenBurns.frameSize = [256 256]; % [height width]
 KenBurns.duration = 2; % [s]
 KenBurns.startRect = [1 1 1]; % x, y, scale
@@ -28,43 +27,52 @@ KenBurns.translation = KenBurns.translationLin;
 
 % demo
 clf;
-imshow(Image); 
-colormap gray; 
-axis image;
+KenBurns.image();
 KenBurns.plot();
 
-% Write to output file
+% write
 KenBurns.make();
 
-%% Chaining of translation effects
+%% Chaining of transition effects
+Image = imread('cameraman.tif');
 videoWriter = VideoWriter('kenburns_chained','MPEG-4');
 KenBurns = KenburnsObj(videoWriter, Image);
-KenBurns.translation = @(t) KenBurns.translationBackForth(KenBurns.translationSin(t));
+KenBurns.translation = @(t) KenBurns.translationCos(KenBurns.translationBackForth(t));
 
 % demo
 clf;
-imshow(Image); 
-colormap gray; 
-axis image;
+KenBurns.image();
 KenBurns.plot();
+
+% write
+KenBurns.make();
+
+%% For small images
+% For small images it can be advantageous to upsample the image before 
+% before creating the KenBurnsObj. This prevents aliasing of the image.
+Image = imread('cameraman.tif');
+Image = imresize(Image, 2);
+videoWriter = VideoWriter('kenburns_upsampled','MPEG-4');
+KenBurns = KenburnsObj(videoWriter, Image);
 
 KenBurns.make();
 
 %% For very large images
-% the default 'translation' method is computationally expensive 
-% this can be reduced at the expense of some aliasing at high zoom levels
+% For very large images the default 'translation' method is computationally expensive 
+% this can be reduced at the expense of some 'shaking' at high zoom levels
 % by setting KenBurns.method = 'crop';
-
+Image = imread('cameraman.tif');
 videoWriter = VideoWriter('kenburns_crop','MPEG-4');
 KenBurns = KenburnsObj(videoWriter, Image);
-KenBurns.duration = 3;
 KenBurns.method = 'crop';
 
 % demo
-% don't image the Canvas, just the crops
+% don't show the Image, just the crops
 clf;
-colormap gray; 
-axis image;
 KenBurns.plot();
 
+% write
 KenBurns.make();
+
+
+
